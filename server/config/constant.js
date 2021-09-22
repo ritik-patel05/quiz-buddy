be8fe792fab6cb8acd77e105096365a3fadabd21
unknown <ritikpatel05121999@gmail.com>
2021-09-22 20:22:25 +0530

    refactor(server): change constant name

--- server/config/constant.js
+++ server/config/constant.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 
 module.exports = {
   jwt: {
-    JWT_SECRET: process.env.JWT_SECRET,
-    JWT_EXPIRY: process.env.JWT_EXPIRY,
+    JWT_SECRET: process.env.JWT_ACCESS_SECRET,
+    JWT_EXPIRY: process.env.JWT_ACCESS_EXPIRY,
     JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
     JWT_REFRESH_EXPIRY: process.env.JWT_REFRESH_EXPIRY,
   },
