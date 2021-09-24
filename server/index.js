460fd354fdb6f1eceb87af8234070336e2e10d0a
unknown <ritikpatel05121999@gmail.com>
2021-09-24 10:39:29 +0530

    fix(server): allow cors from client with credentials

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const compression = require("compression");
 const helmet = require("helmet");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const initializeDBConnection = require("./config/db.connect");
 const errorHandlerRoute = require("./middlewares/errorHandler");
 const notFoundHandlerRoute = require("./middlewares/routeHandler");
@@ -15,7 +16,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet()); // Secure our Express apps by setting various HTTP headers.
 app.use(compression()); // Compress routes
-app.use(cors());
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cookieParser()); // Populate req.cookies with cookies
 
 // Connect to Database.
 initializeDBConnection();
