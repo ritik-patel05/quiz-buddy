bb424d0de304bd901afd43d713c5eb4cfa8a558f
unknown <ritikpatel05121999@gmail.com>
2021-11-28 17:57:29 +0530

    fix: logout button if user logged in, mobile view

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -154,32 +154,45 @@ export const Header = () => {
                     Dashboard
                   </Link>
                 </li>
-                <li>
-                  <Link
-                    to="/login"
-                    className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex justify-center w-full transition duration-150 ease-in-out"
-                  >
-                    Sign In
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/signup"
-                    className="btn-sm flex w-full my-2 py-2 px-4 text-gray-200 bg-gray-900 hover:bg-gray-800"
-                  >
-                    <span>Sign up</span>
-                    <svg
-                      className="w-3 h-3 fill-current text-gray-400 flex-shrink-0 ml-2 -mr-1"
-                      viewBox="0 0 12 12"
-                      xmlns="http://www.w3.org/2000/svg"
+                {!name ? (
+                  <>
+                    <li>
+                      <Link
+                        to="/login"
+                        className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex justify-center w-full transition duration-150 ease-in-out"
+                      >
+                        Sign In
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/signup"
+                        className="btn-sm flex w-full my-2 py-2 px-4 text-gray-200 bg-gray-900 hover:bg-gray-800"
+                      >
+                        <span>Sign up</span>
+                        <svg
+                          className="w-3 h-3 fill-current text-gray-400 flex-shrink-0 ml-2 -mr-1"
+                          viewBox="0 0 12 12"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z"
+                            fillRule="nonzero"
+                          />
+                        </svg>
+                      </Link>
+                    </li>
+                  </>
+                ) : (
+                  <li>
+                    <button
+                      onClick={logoutHandler}
+                      className="btn-sm flex w-full my-2 py-2 px-4 text-gray-200 bg-gray-900 hover:bg-gray-800"
                     >
-                      <path
-                        d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z"
-                        fillRule="nonzero"
-                      />
-                    </svg>
-                  </Link>
-                </li>
+                      Log Out
+                    </button>
+                  </li>
+                )}
               </ul>
             </nav>
           </div>
